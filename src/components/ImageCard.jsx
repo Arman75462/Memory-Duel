9a3f0490a2956ae2394b2577dafd24812a261caa
Arman <armangulistani@gmail.com>
2024-11-14 20:26:48 -0500

    Add cards slight rotation
    To make the cards' placement seem more realistic

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -7,8 +7,25 @@ function ImageCard({
   ImageCardBack_BackgroundColor,
   onClick,
 }) {
+  // Determine rotation angle based on the card's id
+  function getRotationAngle(id) {
+    // If number is even
+    if (id % 2 === 0) {
+      return "-5deg";
+    } else {
+      return "5deg";
+    }
+  }
+
   return (
-    <div className="ImageCard" id={id} onClick={onClick}>
+    <div
+      className="ImageCard"
+      id={id}
+      onClick={onClick}
+      style={{
+        transform: `rotate(${getRotationAngle(id)})`, // Apply the calculated rotation
+      }}
+    >
       <div className="ImageCard__front" id={id}>
         {/* The 'id' is also assigned to ImageCard__front to ensure that the flipping action is triggered from anywhere on the card, not just the borders. Without passing the id to ImageCard__front, clicking on the center part of the card, which corresponds to ImageCard__front, wouldn't activate the flip. */}
       </div>
